c853e7bb6dc3ad7791b670bcf9a7ae0bc284477d
Markus-E <github@ehmig.net>
2017-03-15 22:28:18 +0100

    Update admin.js
    fix for https://s.mydomain.de/?SHORTCODE

--- js/admin.js
+++ js/admin.js
@@ -10,10 +10,6 @@ function ocUrl(url) {
 $(document).ready(function() {
 	$('#shorten-host-url').change(function() {
 		var val = $(this).val();
-		if (endsWith(val, "/")) {
-			val = val.substring(0, val.length-1);
-			$(this).val(val);
-		}
 	        $.post(ocUrl("setval"), { host: val }, function (data) {
 			 console.log('response', data);
         	});
